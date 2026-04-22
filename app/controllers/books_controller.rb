796b00bff246c3bee167d9a12698c8043bced7c9
soham <soham777chavan777@gmail.com>
2026-04-22 19:56:38 +0530

    update ui

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
   before_action :set_categories, only: %i[new edit create update]
+  before_action :authenticate_user!, only: %i[new create]
 
   # GET /books
   def index
